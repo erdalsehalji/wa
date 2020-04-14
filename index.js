2dad65630bee997c586496d9e13846ed5b32e21c
erdalsehalji <58055108+erdalsehalji@users.noreply.github.com>
2020-04-14 13:53:58 +0200

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const app = express()  // instanciranje aplikacije 
-const port = 3000  // port na kojem će web server slušati
-let lista = []
-app.get('/add', (req, res) => {
-    let b = Math.floor(Math.random() * 100) +1
-    return lista.push(b)
-});
-app.listen(port, () => console.log(`Slušam na portu ${port}!`))
